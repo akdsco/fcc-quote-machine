7288b7a36f07955d954007bb85b1ed2635af07bd
Arkadiusz <arkadiusz@ostrowski.co>
2020-02-25 10:17:23 +0000

    write initially working application - install randomcolor & styled components dependencies - add css for category buttons and credits - create share links for twitter and tumblr

--- src/App.js
+++ src/App.js
@@ -1,13 +1,117 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Box from "./Components/Box";
+import styled, {keyframes} from "styled-components";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHashtag} from "@fortawesome/free-solid-svg-icons";
+
+const randomColor = require('randomcolor');
 
 function App() {
+  const [color, setColor] = useState(randomColor());
+  const [quote, setQuote] = useState('');
+  const [category, setCategory] = useState('');
+
+  useEffect(() => {
+    let startingColor = randomColor();
+    setColor({
+      currentColor: startingColor,
+      prevColor: startingColor
+    });
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = () => {
+    let url = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setQuote(data[0]));
+  };
+
+  const handleColorChange = () => {
+    setColor(prevState => ({
+      currentColor: randomColor(),
+      prevColor: prevState.currentColor
+    }));
+    fetchQuote();
+  };
+
+  const handleCategory = ( e, name ) => {
+    e.preventDefault();
+    setCategory(name);
+  };
+
+  const keyFrame = keyframes`
+  0% {
+    background-color: ${color.prevColor};
+  }
+  100% {
+    background-color: ${color.currentColor};
+  }
+  `;
+
+  const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    color: ${color};
+    animation: ${keyFrame} 2s forwards;
+  `;
+
+  const borderKeyFrame = keyframes`
+  0% {
+    border-bottom-color: ${color.prevColor};
+  }
+  100% {
+    border-bottom-color: ${color.currentColor};
+  }
+  `;
+
+  const StyledButton = styled.button`
+    border-bottom: 3px solid ${color.currentColor};
+    animation: ${borderKeyFrame} 2s forwards;
+  `;
+
   return (
-    <div className="App">
-      <Box />
-    </div>
+    quote &&
+      <StyledDiv className="App">
+        <div className='category-group'>
+          {category === 'computer' ?
+            <button className='category-button selected-button'>
+              <FontAwesomeIcon size='sm' icon={faHashtag}/>
+              computer speaks
+            </button>
+            :
+            <StyledButton
+              className={`category-button`}
+              onClick={e => handleCategory(e, 'computer')}
+            >
+              <FontAwesomeIcon size='sm' icon={faHashtag}/>
+              computer speaks
+            </StyledButton>}
+          {category === 'breakingBad' ?
+            <button className='category-button selected-button'>
+              <FontAwesomeIcon size='sm' icon={faHashtag}/>
+              breaking bad
+            </button>
+            :
+            <StyledButton
+              className={`category-button`}
+              onClick={e => handleCategory(e, 'breakingBad')}
+            >
+              <FontAwesomeIcon size='sm' icon={faHashtag} />
+              breaking bad
+            </StyledButton>
+          }
+        </div>
+        <Box
+          bgColorChange={handleColorChange}
+          color={color}
+          keyFrame={keyFrame}
+          quote={quote}
+        />
+        <p className='credits'>by <a href='http://arkadiusz.tech'>akds.tech</a></p>
+      </StyledDiv>
   );
 }
 
-export default App;
+export default App
--- src/Components/Box.js
+++ src/Components/Box.js
@@ -2,21 +2,79 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTumblr, faTwitter} from "@fortawesome/free-brands-svg-icons";
 import {faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
+import styled, {keyframes} from "styled-components";
+import {StyledSocialBtn} from "./StyledComponents";
 
-function Box() {
+function Box(props) {
+
+  // const StyledSocialBtn = styled.a`
+  //   color: ${props.color.currentColor};
+  //   animation: ${props.keyFrame} 2s forwards;
+  // `;
+
+  const StyledButton = styled.button`
+    background-color: ${props.color.currentColor};
+    animation: ${props.keyFrame} 2s forwards;
+  `;
+
+  const textKeyFrame = keyframes`
+  0% {
+    color: ${props.color.prevColor};
+  }
+  100% {
+    color: ${props.color.currentColor};
+  }
+  `;
+
+  const StyledQuote = styled.h1`
+    color: ${props.color};
+    animation: ${textKeyFrame} 2s forwards;
+  `;
+
+  const StyledAuthor = styled.p`
+    color: ${props.color};
+    animation: ${textKeyFrame} 2s forwards;
+  `;
+
+  const htmlReadyText = (data) => {
+    if(data) {
+      return data.replace(/\s/g, "%20");
+    }
+    return '';
+  };
+
+  const {author, quote} = props.quote;
 
   return(
-    <div id='quote-box'>
+    <div>
+      <div id='quote-box'>
+        <StyledQuote id='text'><FontAwesomeIcon icon={faQuoteLeft} size='sm' className='quote-icon' />{quote}</StyledQuote>
+        <StyledAuthor id='author'> - {author}</StyledAuthor>
 
-      <h1 id='text'><FontAwesomeIcon icon={faQuoteLeft} size='sm' className='quote-icon' />Sample quote from someone enlightened</h1>
-      <p id='author'> - Authors Name</p>
-
-      <div className='control-panel flex'>
-        <div className='flex'>
-          <a href='http://link.com' className='social-share-btn' id='tweet-quote'><FontAwesomeIcon size='md' className='social-icon' icon={faTwitter} /></a>
-          <a href='http://link.com' className='social-share-btn' id='tumbler-quote'><FontAwesomeIcon size='md' className='social-icon' icon={faTumblr} /></a>
+        <div className='control-panel flex'>
+          <div className='flex'>
+            <StyledSocialBtn
+              href={`https://twitter.com/intent/tweet?hashtags=quotes
+              &related=freecodecamp
+              &text="${htmlReadyText(quote)}"%20${htmlReadyText(author)}`}
+              className='social-share-btn'
+              id='tweet-quote'>
+              <FontAwesomeIcon size='1x' className='social-icon' icon={faTwitter} />
+            </StyledSocialBtn>
+            <StyledSocialBtn
+              href={`https://www.tumblr.com/widgets/share/tool?posttype=quote
+              &tags=quotes,freecodecamp
+              &caption=${htmlReadyText(author)}
+              &content=${htmlReadyText(quote)}
+              &canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`}
+              className='social-share-btn'
+              id='tumbler-quote'
+            >
+              <FontAwesomeIcon size='1x' className='social-icon' icon={faTumblr} />
+            </StyledSocialBtn>
+          </div>
+          <StyledButton onClick={props.bgColorChange} id='new-quote' className='new-quote-btn'>Next quote</StyledButton>
         </div>
-        <button id='new-quote' className='new-quote-btn'>New quote</button>
       </div>
     </div>
   )
