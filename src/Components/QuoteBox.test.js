a0cbb9cdd05816859ade5f9dacfd529fdbba1f94
Arkadiusz <arkadiusz@ostrowski.co>
2020-08-04 09:45:04 +0100

    install firebase, deploy app

--- src/Components/QuoteBox.test.js
+++ src/Components/QuoteBox.test.js
@@ -58,3 +58,36 @@ test('renders tumblr share button', () => {
   const component = wrapper.find('#tumbler-quote');
   expect(component.length).toBe(1);
 });
+
+test("htmlLink() replaces all spaces to html sign", () => {
+  const component = wrapper.find('#tumbler-quote');
+});
+
+test("renders new quote button", () => {
+  const component =  wrapper.find('#new-quote');
+  expect(component.length).toBe(1);    const wrapper = setup();
+
+});
+
+describe('testing functionality of new quote btn', () => {
+  const mockSetCurrentGuess = jest.fn();
+  let wrapper;
+
+  beforeEach( () => {
+    mockSetCurrentGuess.mockClear();
+    React.useState = jest.fn( () => ['', mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  test('changes to new quote after click', () => {
+    const newQuoteBtn = wrapper.find('#new-quote');
+
+    const mockEvent = {
+      quote: "Test quote",
+      author: 'Test'
+    };
+    newQuoteBtn.simulate('click');
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith({quote: 'Test quote', author: 'Tes'});
+  });
+});
